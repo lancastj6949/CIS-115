8f4a18199f2636a807477e6bfafcc6882b5dbe28
lancastj6949 <lancastj6949@student.faytechcc.edu>
2026-02-20 16:57:23 +0000

    Finished M3CW1

--- M3CW1_Lancaster.java
+++ M3CW1_Lancaster.java
@@ -5,16 +5,16 @@ public class M3CW1_Lancaster
 {
    public static void main(String[] args)
     { 
-      System.out.println("First Name"); // replace first name with your first name
-      System.out.println("Last Name"); // title of following course schedule
-      System.out.println("2025 Fall Schedule"); // title of following course schedule
-      System.out.println("List course prefix, number and section as well as course title");
+      System.out.println("John"); // replace first name with your first name
+      System.out.println("Lancaster"); // title of following course schedule
+      System.out.println("2026 Spring Schedule"); // title of following course schedule
+      System.out.println("CIS-115-0001-0901.2026SP");
       // example would be CIS-115-0001 Intro to Programming and Logic
       // repeat as needed for each course
-      System.out.println("List course prefix, number and section as well as course title");
-      System.out.println("List course prefix, number and section as well as course title");
-      System.out.println("List course prefix, number and section as well as course title");
-      System.out.println("State your programming experience and what if any language you know");
+      System.out.println("PSY-150-0005.2026SP");
+      System.out.println("MAT-172-5P01.2026SP");
+      System.out.println("Biology Honors-Cains");
+      System.out.println("Some experience using Scratch but do not know much language");
 
     }
 }
