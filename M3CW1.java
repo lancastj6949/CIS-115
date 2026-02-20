6be1f4923a815ea7d12cbf3a3e2b62a81a458c74
lancastj6949 <lancastj6949@student.faytechcc.edu>
2026-02-20 16:44:28 +0000

    M3CW1

---
+++ M3CW1.java
@@ -0,0 +1,21 @@
+// M3CW1 Assignment
+// your class name should match your file name
+// Be sure to make this happen when you create the java file
+public class M3CW1_Lanca
+{
+   public static void main(String[] args)
+    { 
+      System.out.println("First Name"); // replace first name with your first name
+      System.out.println("Last Name"); // title of following course schedule
+      System.out.println("2025 Fall Schedule"); // title of following course schedule
+      System.out.println("List course prefix, number and section as well as course title");
+      // example would be CIS-115-0001 Intro to Programming and Logic
+      // repeat as needed for each course
+      System.out.println("List course prefix, number and section as well as course title");
+      System.out.println("List course prefix, number and section as well as course title");
+      System.out.println("List course prefix, number and section as well as course title");
+      System.out.println("State your programming experience and what if any language you know");
+
+    }
+}
+
