fce3141c609703a16acf78af5d7100004e7f0dff
lancastj6949 <lancastj6949@student.faytechcc.edu>
2026-04-04 16:44:50 +0000

    Worked on M5HW1_Lancaster.java

---
+++ M5HW1_Lancaster.java
@@ -0,0 +1,17 @@
+// John Lancaster
+// M5HW1
+// 4/4/2026
+// This code calculates a students average grade and letter grade 
+
+
+import java.util.Scanner;
+public class M5HW1_Lancaster 
+{
+    
+    public static void main(String[] args)
+    {
+        Double first_grade; double average = 90;
+
+    }
+
+}
