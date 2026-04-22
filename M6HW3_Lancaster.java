ab7993c2d1fb9befe36a775f70d40291d9b48833
lancastj6949 <lancastj6949@student.faytechcc.edu>
2026-04-22 22:00:18 +0000

    WorkedM6HW3_Lancaster.java

---
+++ M6HW3_Lancaster.java
@@ -0,0 +1,22 @@
+// John Lancaster
+// M6HW3
+// 4/22/2026
+// Calculates a employee's monthly commission for widget sales
+
+
+public class M6HW3_Lancaster 
+{
+    public static void main(String[] args)
+    {
+        String salesperson_name
+        double widgets_sold, widgets_returned, net_widgets, commission;
+        System.out.print("Enter the salespersons name: ");
+        salesperson_name = k.nextline();
+        System.out.print("Enter the widgets sold: ");
+        widgets_sold = k.nextdouble();
+        System.out.print("Enter the widgets returned: ");
+        widgets_returned = k.nextdouble();
+        net_widgets = (widgets_sold - widgets_returned);
+        commission = $500+(net_widgets*$0.10);
+    }
+}
